38f7104785cac5e921d447789be22461a5bc81d3
Harris <matthew.d.harris@aecom.com>
2019-11-05 21:31:22 -0500

    added 'id' slot to Box class

--- R/objects_definition.R
+++ R/objects_definition.R
@@ -1,11 +1,11 @@
 #' An s4 class to represent a Container
 #'
-#' @slot origin A length-three vector 
+#' @slot origin A length-three vector
 #' @slot length A numeric
 #' @slot height A numeric
 #' @slot width A numeric
 #'
-#' @examples 
+#' @examples
 #' # create a container with size 2 x 2 x 2
 #' c1 <- Container(length = 2, height = 2, width = 2)
 #'
@@ -15,7 +15,7 @@ Container <- setClass('Container',
                                 length = 'numeric',
                                 height = 'numeric',
                                 width = 'numeric',
-                                ems = 'list'  # list of instances of class EMS 
+                                ems = 'list'  # list of instances of class EMS
                                 ),
                       prototype = list(origin = c(0, 0, 0)),
                       validity = function (object) {  # make sure that all parameters are positive
@@ -26,15 +26,15 @@ Container <- setClass('Container',
                           }
                       )
 
-# create method 'initialize' for Container class to create 
+# create method 'initialize' for Container class to create
 # slot EMS (initial EMS is whole Container) when
-# an instance of class Container is being created 
-#' @export 
-setMethod('initialize', 
+# an instance of class Container is being created
+#' @export
+setMethod('initialize',
           'Container',
           function (.Object, ...) {
               .Object <- callNextMethod()
-              .Object@ems <- 
+              .Object@ems <-
                   list(
                       EMS(origin = .Object@origin,
                           length = .Object@length,
@@ -48,13 +48,14 @@ setMethod('initialize',
 
 #' An s4 class to represent a Box
 #'
-#' @slot origin A length-three vector 
+#' @slot origin A length-three vector
 #' @slot length A numeric
 #' @slot height A numeric
 #' @slot width A numeric
 #' @slot weight A numeric
+#' @slot id A chracter string
 #'
-#' @examples 
+#' @examples
 #' # create a box with size 1 x 1 x 1
 #' b1 <- Box(length = 1, height = 1, width = 1)
 #'
@@ -63,16 +64,17 @@ Box <- setClass('Box',
                 slots = c(origin = 'numeric',
                           length = 'numeric',
                           height = 'numeric',
-                          width = 'numeric',
+                          width  = 'numeric',
+                          id     = 'character',
                           weight = 'numeric'
                           ),
-                validity = function (object) {  # make sure that all parameters are positive
-                        if(object@length <= 0 | object@height <= 0 | object@width <= 0) {
+                validity = function(object){  # make sure that all parameters are positive
+                        if(object@length <= 0 | object@height <= 0 | object@width <= 0){
                             return("A number <= 0 for one of the parameters was given.")
                         }
                         return(TRUE)
                     }
-                ) 
+                )
 
 # define class for Empty Maximal Spaces
 #' @export EMS
